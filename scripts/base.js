02ad8aa56d42d81e2663563e1422cc50a87728b0
Steven Senkus <steven.senkus@yahoo.com>
2013-06-15 13:55:01 -0700

    Loaded content formatting

--- scripts/base.js
+++ scripts/base.js
@@ -1,6 +1,14 @@
 	function ajaxLoad() {
 	    var ed = tinyMCE.get('content');
-	    ed.setContent('<div style="text-align:center"><h1>AWESOME PICTURE</h1><img style="width: 200px; height: 200px;" src="images/mount_hood_USGS.jpg" /><br /><p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p></div>');
+	    ed.setContent(	'<div style="text-align:center">' + 
+							'<h1>AWESOME PICTURE</h1>
+							'<img style="width: 200px; height: 200px;" src="images/mount_hood_USGS.jpg" /><br />' +
+							'<p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor ' + 
+							'incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation' + 
+							' ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in' + 
+							' voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat' +
+							' non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>' + 
+						'</div>');
 	}
 
 	function ajaxSave() {
